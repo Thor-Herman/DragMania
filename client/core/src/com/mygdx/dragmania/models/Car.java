59f08e13afa06c4ad842bd15da3d0e31e8757ef5
Stian Nyblom <stianeny@stud.ntnu.no>
2021-04-01 17:27:52 +0200

    Refactor Car

--- client/core/src/com/mygdx/dragmania/models/Car.java
+++ client/core/src/com/mygdx/dragmania/models/Car.java
@@ -1,54 +1,40 @@
 package com.mygdx.dragmania.models;
 
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.graphics.Texture;
-
 public class Car {
 	
-    private Vector2 position;
-    private Vector2 velocity;
-    private Texture carTexture;
-    private static final int MAXVELOCITY = 5;
+    private int position;
+    private int velocity;
+    private int maxVelocity;
     
-    public Car() {
-        this.position = new Vector2(0, 0);
-        this.velocity = new Vector2(0, 0);
+    public Car(int maxVelocity) {
+        this.position = 0;
+        this.velocity = 0;
+        this.maxVelocity = maxVelocity;
     }
 
-	// Accelerates when direction is positive. Deaccelerates when direction is negative.
-    public void accelerate(int direction) {
-        if (direction > 0 && velocity.y < MAXVELOCITY) {
-            velocity.add(0, 1);
-        } else if (direction < 0 && velocity.y > 0) {
-            velocity.add(0, -1);
+	// Accelerates when forward boolean is true. Decelerates when false.
+    public void accelerate(boolean forward) {
+        if (forward && velocity < maxVelocity) {
+            velocity++;
+        } else if (!forward && velocity > 0) {
+            velocity--;
         }
     }
     
     public void update(float dt, boolean isTouching) {
-        if (isTouching) {
-            accelerate(1);
-        } else {
-            accelerate(-1);            
-        }
-
-        position.add(velocity.x*dt, velocity.y*dt);
+        accelerate(isTouching);
+        position += velocity*dt;
     }
 
-    public Texture getTexture() {
-        return this.carTexture;
-    }
-    
-    public Vector2 getPosition() {
+    public int getPosition() {
         return this.position;
     }
-
-    public Vector2 getVelocity() {
+    
+    public int getVelocity() {
         return this.velocity;
     }
-    
-    @Override
-    public String toString() {
-        return "Pos: " + getPosition().y + ", velocity: " + getVelocity().y;
+
+    public int getMaxVelocity() {
+        return this.maxVelocity;
     }
 }
