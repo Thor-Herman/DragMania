997576ab74d1842f170a8cc92569f4f3d4d67041
Tommy Woldseth <tommyew@stud.ntnu.no>
2021-04-04 11:33:27 +0200

    feat: Worked on policeman turning and time penalty as well as animation

--- client/core/src/com/mygdx/dragmania/models/Policeman.java
+++ client/core/src/com/mygdx/dragmania/models/Policeman.java
@@ -15,6 +15,10 @@ public class Policeman {
     private Car car;
     private int currentTurnPositionIndex;
     private int turnTimeCounter;
+    private long policemanTurnStart;
+    private long policemanTurnStop;
+    private long timePenaltyStart;
+    private long timePenaltyStop;
     // Not correct position
     public static final int xPos = 200;
     public static final int yPos = 800;
@@ -28,6 +32,7 @@ public class Policeman {
         this.car = car;
         currentTurnPositionIndex = 0;
         turnTimeCounter = 0;
+        policemanTurnStart = 0;
     }
 
     public void update(float dt) {
@@ -44,13 +49,44 @@ public class Policeman {
     }
 
     public void checkIfAllowedToDrive() {
-        if (car.getVelocity() > 0 && hasTurned) {
+        checkTimePenalty();
+        checkPolicemanTurn();
+
+    }
+
+    public void checkPolicemanTurn() {
+        // When policeman turns towards car, start timer and prevent car from driving
+        if (hasTurned && policemanTurnStart == 0) {
             car.canDrive(false);
-            //Start counter
+            policemanTurnStart = System.currentTimeMillis();
         }
 
-        //if counter > 2sek && !allowedtodrive
-        //allow to drive
+        policemanTurnStop = System.currentTimeMillis();
+
+        // Check if two seconds has gone since policeman turn
+        // Allow car to drive again
+        if (policemanTurnStart > 0 && (policemanTurnStop-policemanTurnStart) > 2000) {
+            toggleTurn();
+            car.canDrive(true);
+            policemanTurnStart = 0;
+        }
+    }
+
+    public void checkTimePenalty() {
+        // Start timer for the duration of the policeman turn
+        if (car.getVelocity() > 0 && hasTurned && timePenaltyStart == 0) {
+            car.canDrive(false);
+            timePenaltyStart = System.currentTimeMillis();
+        }
+
+        timePenaltyStop = System.currentTimeMillis();
+
+        // Check if timepenalty is up
+        if (timePenaltyStart > 0 && (timePenaltyStop-timePenaltyStart) > 4000) {
+            car.canDrive(true);
+            timePenaltyStart = 0;
+        }
+
     }
 
     public void toggleTurn() {
@@ -61,8 +97,13 @@ public class Policeman {
         return hasTurned;
     }
 
-    public Texture getAnimation(String animation) {
-        return policemanAnimation.getAnimation(animation);
+    public Texture getAnimation() {
+        if (hasTurned) {
+            return policemanAnimation.getAnimation("TurnedTowards");
+        }
+        else {
+            return policemanAnimation.getAnimation("TurnedAway");
+        }
     }
 
     public Vector2 getPosition() {
