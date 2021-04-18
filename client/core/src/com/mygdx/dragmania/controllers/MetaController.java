eed8323f792139e441b5f7947aa8ecb27261ba69
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-04-18 22:47:56 +0200

    Make MetaController the desktop launcher

--- client/core/src/com/mygdx/dragmania/controllers/MetaController.java
+++ client/core/src/com/mygdx/dragmania/controllers/MetaController.java
@@ -2,7 +2,11 @@ package com.mygdx.dragmania.controllers;
 
 import java.util.Stack;
 
-public class MetaController {
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.utilities.GameClient;
+
+public class MetaController extends Game {
 
     private Stack<Controller> controllers = new Stack<>();
 
@@ -18,10 +22,32 @@ public class MetaController {
     }
 
     public void pop() {
-        controllers.pop();
+        if (controllers.size() > 0) controllers.pop();
     }
 
     public void push(Controller controller) {
         controllers.push(controller);
+    }
+
+    @Override
+    public void create() {
+        LobbyController lobbyController = LobbyController.getInstance();
+        GameController gameController = GameController.getInstance();
+        lobbyController.connectToServer();
+        lobbyController.createGame("TH");
+        try {
+            Thread.sleep(8000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("Create finished");
+    }
+
+    @Override
+    public void render() {
+        System.out.println("Rendering...");
+        float dt = Gdx.graphics.getDeltaTime();
+        GameController.getInstance().update(dt, true);
     }
 }
