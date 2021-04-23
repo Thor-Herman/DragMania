7897b24e38f83c80f4ef9a89e1d1c5b0ffeef064
tommyew97 <44811386+tommyew97@users.noreply.github.com>
2021-04-23 10:29:22 +0200

    feat: Made help view #38 (#54)
    Co-authored-by: Tommy Woldseth <tommyew@stud.ntnu.no>

--- client/core/src/com/mygdx/dragmania/views/HelpView.java
+++ client/core/src/com/mygdx/dragmania/views/HelpView.java
@@ -4,14 +4,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.dragmania.DragMania;
+import com.mygdx.dragmania.views.buttons.NextButton;
+
+import java.util.ArrayList;
 
 public class HelpView extends View {
 
     private BackArrow backArrow;
+    private ArrayList<Texture> helpTextures;
+    private NextButton nextButton;
+    private int textureIndex;
 
     public HelpView(ViewManager viewManager) {
         super(viewManager);
         backArrow = new BackArrow(0,0);
+        helpTextures = new ArrayList<>();
+        helpTextures.add(new Texture("help1.png"));
+        helpTextures.add(new Texture("help2.png"));
+        helpTextures.add(new Texture("help3.png"));
+        nextButton = new NextButton(320, 20);
+        textureIndex = 0;
     }
 
 
@@ -23,9 +35,8 @@ public class HelpView extends View {
     @Override
     public void handleInput() {
         if(Gdx.input.justTouched()) {
-            //System.out.println(Gdx.input.getY());
-            //System.out.println(Gdx.input.getX());
             checkBackTouched(backArrow);
+            checkNextButtonTouched();
         }
     }
 
@@ -35,7 +46,17 @@ public class HelpView extends View {
         update(delta);
         SpriteBatch sb = new SpriteBatch();
         sb.begin();
+        sb.draw(helpTextures.get(textureIndex), 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         sb.draw(backArrow.getBackArrow(), backArrow.getPosition().x, backArrow.getPosition().y, backArrow.getWidth()/3, backArrow.getHeight()/3);
+        if(textureIndex < 2) {
+            sb.draw(nextButton.getButton(), nextButton.getPosition().x, nextButton.getPosition().y);
+        }
         sb.end();
+    }
+
+    public void checkNextButtonTouched() {
+        if(nextButton.getBounds().contains(Gdx.input.getX(), Gdx.graphics.getHeight()-Gdx.input.getY())) {
+            textureIndex++;
+        }
     }
 }
