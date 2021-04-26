90651dde0cedc7c0af0377272580120b25b17cf7
Thor-Herman van Eggelen <44784559+Thor-Herman@users.noreply.github.com>
2021-04-26 12:46:15 +0200

    chore: add env file (#63)
    * Update .gitignore
    
    * Create Env.java
    
    * Create Env.java

---
+++ server/src/com/utilities/Env.java
@@ -0,0 +1,11 @@
+public class Env {
+
+    private static final int TCP_PORT = 54587, UDP_PORT = 54787;
+    public static int getTcpPort() {
+        return TCP_PORT;
+    }
+    public static int getUdpPort() {
+        return UDP_PORT;
+    }
+
+}
