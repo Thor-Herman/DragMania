85b74375548827f9c42584bf828c2112678410b2
Christoffer Synnestvedt <56232993+CSynnestvedt@users.noreply.github.com>
2021-04-26 20:46:58 +0200

    refactor(Env): Remove plain text ip address

--- client/core/src/com/utilities/Env.java
+++ client/core/src/com/utilities/Env.java
@@ -3,7 +3,7 @@ package com.utilities;
 public class Env {
 
     private static final int TCP_PORT = 54587, UDP_PORT = 54787;
-    public static final String  IP_ADDRESS = "13.49.228.177";
+    public static final String  IP_ADDRESS = "";
     public static int getTcpPort() {
         return TCP_PORT;
     }
