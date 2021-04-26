9f8a5ad34393bdda498ca4a2f024ba2627dafa1d
Thor-Herman van Eggelen <thorherman.eggelen@gmail.com>
2021-04-26 12:37:01 +0200

    Create Env.java

---
+++ client/core/src/com/utilities/Env.java
@@ -0,0 +1,15 @@
+package com.utilities;
+
+public class Env {
+
+    private static final int TCP_PORT = 54587, UDP_PORT = 54787;
+    public static final String  IP_ADDRESS = "";
+    public static int getTcpPort() {
+        return TCP_PORT;
+    }
+    public static int getUdpPort() {
+        return UDP_PORT;
+    }
+    public static String getIPAddress() { return IP_ADDRESS; }
+
+}
